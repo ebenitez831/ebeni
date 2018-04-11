68a3363b56be55c38fe5447378d24d2f6947bae3
Esteban Benitez <ebenitez@csumb.edu>
2018-04-11 22:41:44 +0000

    trying to make project launch

--- HW/project/index.php
+++ HW/project/index.php
@@ -16,19 +16,19 @@
     function getDatabaseConnection($dbName) 
     {
         //checks whether the URL contains "herokuapp" (using Heroku)
-        if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-           $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-           $host = $url["host"];
-           $dbname = substr($url["path"], 1);
-           $username = $url["user"];
-           $password = $url["pass"];
-        }
-        else
-        {
+        // if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        //   $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        //   $host = $url["host"];
+        //   $dbname = substr($url["path"], 1);
+        //   $username = $url["user"];
+        //   $password = $url["pass"];
+        // }
+    //    else
+    //    {
             $host = "localhost";
             $username = "web_user"; //make sure to change before pushing web_user, s3cr3t
             $password = "s3cr3t";
-        }
+    //    }
         
         $dbConn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
